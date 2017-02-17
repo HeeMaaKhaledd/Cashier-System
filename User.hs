a59a73a5cc06298b7fcb41f5eff0f77d2e7baf7a
Jesper <saxer@nl119-144-49.student.uu.se>
2017-02-17 14:33:49 +0100

    added funtions to User.hs

--- User.hs
+++ User.hs
@@ -9,17 +9,28 @@ data User = User Name Id Wallet Spent IsAdmin deriving Show
 
 -- a set of functions that is suitable for a user to have.
 -- Set the name of the user.
-setName = undefined
+setName :: Name -> User -> User
+setName x (User c i wallet spent a ) = User x i wallet spent a
 
 -- Set the id of the user
-setId = undefined
+setId :: Id -> User -> User
+setId x (User c i wallet spent a ) = User c x wallet spent a
 
 -- add to wallet
-fillWallet = undefined
-
+fillWallet :: Wallet -> User -> User
+fillWallet x (User c i wallet spent a)
+  | x > 0 = User c i (wallet + x) spent a
+  | otherwise = error "You can't fill with negative currency"
 -- removes from wallet.
-removeWallet = undefined
+removeWallet :: Wallet -> User -> User
+removeWallet x (User c i wallet spent a)
+  | x > wallet = error " You can't remove more than you got! "
+  | x > 0 = User c i (wallet - x) spent a
+  | otherwise = error "Invalid number"
 
+--Clears the wallet
+clearWallet :: User -> User
+clearWallet (User c i wallet spent a) = User c i 0 spent a
 -- Spent (adds to how much the user has spent.)
 addSpent = undefined
 
