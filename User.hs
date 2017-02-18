192a1e61aa1e7af12b75ed10f7393f648dc22693
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-18 18:22:58 +0100

    added cart to user.hs

--- User.hs
+++ User.hs
@@ -1,37 +1,39 @@
 -- improving readability of code.
+import Cart
+
 type Name = String
 type Id = Int
 type Wallet = Int
 type Spent = Int
 type IsAdmin = Bool
 
-data User = User Name Id Wallet Spent IsAdmin deriving Show
+data User = User Name Id Wallet Spent IsAdmin Cart deriving Show
 
 -- a set of functions that is suitable for a user to have.
 -- Set the name of the user.
 setName :: Name -> User -> User
-setName x (User c i wallet spent a ) = User x i wallet spent a
+setName x (User c i wallet spent a k) = User x i wallet spent a k
 
 -- Set the id of the user
 setId :: Id -> User -> User
-setId x (User c i wallet spent a ) = User c x wallet spent a
+setId x (User c i wallet spent a k) = User c x wallet spent a k
 
 -- add to wallet
 fillWallet :: Wallet -> User -> User
-fillWallet x (User c i wallet spent a)
-  | x > 0 = User c i (wallet + x) spent a
+fillWallet x (User c i wallet spent a k)
+  | x > 0 = User c i (wallet + x) spent a k
   | otherwise = error "You can't fill with negative currency"
 
 -- removes from wallet.
 removeWallet :: Wallet -> User -> User
-removeWallet x (User c i wallet spent a)
+removeWallet x (User c i wallet spent a k)
   | x > wallet = error " You can't remove more than you got! "
-  | x > 0 = User c i (wallet - x) spent a
+  | x > 0 = User c i (wallet - x) spent a k
   | otherwise = error "Invalid number"
 
 --Clears the wallet
 clearWallet :: User -> User
-clearWallet (User c i wallet spent a) = User c i 0 spent a
+clearWallet (User c i wallet spent a k) = User c i 0 spent a k
 
 -- Spent (adds to how much the user has spent.)
 addSpent = undefined
