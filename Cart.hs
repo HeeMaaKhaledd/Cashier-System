82a1d79356735b83453b5ef1217e25391bc178f1
Grim Mostrom <grimmostrom@ext149-89.eduroam.uu.se>
2017-02-20 14:49:31 +0100

    finished Cart

--- Cart.hs
+++ Cart.hs
@@ -7,10 +7,17 @@ empty :: Cart
 empty = []
 
 addToCart :: Item -> Cart -> Cart
-addToCart x c = undefined
+addToCart i c = [i] ++ c
 
 removeFromCart :: Item -> Cart -> Cart
-removeFromCart x c = undefined
+removeFromCart i [] = error "non existing product"
+removeFromCart i c = removeFromCartAUX i c Cart.empty
+
+removeFromCartAUX :: Item -> Cart -> Cart -> Cart
+removeFromCartAUX i (c:cs) newC
+  | i == c = newC ++ cs
+  | i /= c = removeFromCartAUX i cs (c:newC) 
 
 calculatePrice :: Cart -> Int
-calculatePrice (c:cs) = undefined
+calculatePrice [] = 0
+calculatePrice (c:cs) = getPrice c + calculatePrice cs
