af82d495cbd1acdfda8daa7ab98436f6865ed62f
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-20 13:50:58 +0100

    added get functions in Item

--- Item.hs
+++ Item.hs
@@ -15,6 +15,9 @@ createItem a b c d = Item a b c d
 setName :: Name -> Item -> Item
 setName x (Item name ean price stock) = Item x ean price stock
 
+getName :: Item -> Name
+getName (Item name ean price stock) = name
+
 -- set the product ean-code
 setEan :: Ean -> Item -> Item
 setEan x (Item name ean price stock) = Item name x price stock
@@ -37,5 +40,8 @@ removeFromStock x (Item name ean price stock)
   | x > 0 && x < stock = Item name ean price (stock - x)
   | x > stock = error "You can't remove more items than we have in stock."
   | otherwise = error "You can't remove a negative number."
+
+getStock :: Item -> Stock
+getStock (Item name ean price stock) = stock
 
 empty = Item "" 0 0 0
