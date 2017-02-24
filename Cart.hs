f5b6059753b4717780787badee78a7d815933be2
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-24 10:49:28 +0100

    added Codingconvention

--- Cart.hs
+++ Cart.hs
@@ -4,24 +4,54 @@ import Database
 
 type Cart = [Item]
 
+{- empty
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 empty :: Cart
 empty = []
 
+{- addToCart
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 addToCart :: Item -> Cart -> Cart
 addToCart i c = [i] ++ c
 
+{- removeFromCart
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 removeFromCart :: Item -> Cart -> Cart
 removeFromCart i [] = error "non existing product"
 removeFromCart i c = removeFromCartAUX i c Cart.empty
 
+{- removeFromCartAUX
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 removeFromCartAUX :: Item -> Cart -> Cart -> Cart
 removeFromCartAUX i (c:cs) newC
   | i == c = newC ++ cs
   | i /= c = removeFromCartAUX i cs (c:newC)
 
+  {- calculatePrice
+     PRE:  True
+     POST: Returns the argument name
+     SIDE EFFECTS: ... if any, including exceptions ...
+  -}
 calculatePrice :: Cart -> Int
 calculatePrice [] = 0
 calculatePrice (c:cs) = getPrice c + calculatePrice cs
 
+{- getFirst
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 getFirst :: Cart -> (Item,Cart)
 getFirst (c:cs) = (c,cs)
--- User.hs
+++ User.hs
@@ -11,58 +11,113 @@ type IsAdmin = Bool
 
 data User = Void | User Name Id Wallet Spent IsAdmin deriving (Show, Eq)
 
+{- newUser
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 newUser :: Name -> Id -> Wallet -> Spent -> IsAdmin -> User
 newUser a b c d e = User a b c d e
 
--- a set of functions that is suitable for a user to have.
--- Set the name of the user.
+{- setName
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 setName :: Name -> User -> User
 setName x (User c i wallet spent a ) = User x i wallet spent a
 
+{- getName
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 getName :: User -> Name
 getName (User c i wallet spent a ) = c
 
--- Set the id of the user
+{- setId
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 setId :: Id -> User -> User
 setId x (User c i wallet spent a ) = User c x wallet spent a
 
+{- getId
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 getId :: User -> Id
 getId (User c i wallet spent a ) = i
 
--- add to wallet
+{- fillWallet
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 fillWallet :: Wallet -> User -> User
 fillWallet x (User c i wallet spent a )
   | x > 0 = User c i (wallet + x) spent a
   | otherwise = error "You can't fill with negative currency"
 
--- removes from wallet.
+{- removeWallet
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 removeWallet :: Wallet -> User -> User
 removeWallet x (User c i wallet spent a )
   | x > wallet = error " You can't remove more than you got! "
   | x > 0 = User c i (wallet - x) spent a
   | otherwise = error "Invalid number"
 
---Clears the wallet
+{- clearWallet
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 clearWallet :: User -> User
 clearWallet (User c i wallet spent a ) = User c i 0 spent a
 
+{- getWallet
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 getWallet :: User -> Wallet
 getWallet (User c i wallet spent a ) = wallet
 
--- Spent (adds to how much the user has spent.)
+{- addSpent
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 addSpent :: Spent -> User -> User
 addSpent x (User c i wallet spent a)
   | x > 0 = User c i wallet (spent + x) a
   | x < 0 = error "The value must be greater than zero"
 
--- Spent (removes to how much the user has spent.)
+{- removeSpent
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 removeSpent :: User -> User
 removeSpent (User c i wallet spent a) = User c i wallet 0 a
 
--- makes user a superuser.
+{- makeAdmin
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 makeAdmin ::User -> User
 makeAdmin (User c i wallet spent a) = User c i wallet spent True
 
--- makes a superuser a normal noob user
+{- removeAdmin
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 removeAdmin ::User -> User
 removeAdmin (User c i wallet spent a) = User c i wallet spent False
