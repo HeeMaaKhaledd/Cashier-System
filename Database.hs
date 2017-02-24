1e3b58789c08fb2afee7d438d2b1e1a5da3ad4d1
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-24 11:21:42 +0100

    added codeconv to Database

--- Database.hs
+++ Database.hs
@@ -10,9 +10,19 @@ type Id = Int -- eanCode for item, userId for users...
 
 type Database a = [(a,Id)]
 
+{- empty
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 empty :: Database a
 empty = []
 
+{- deleteWithID
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 deleteWithID :: Id -> Database a -> Database a
 deleteWithID i [] = error "not in Database"
 deleteWithID i xs = deleteWithIDAUX i xs []
@@ -20,6 +30,11 @@ deleteWithID i xs = deleteWithIDAUX i xs []
           | j == (snd y) = n ++ ys
           | otherwise = deleteWithIDAUX j ys (n++[y])
 
+{- delete
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 delete :: Eq a => a -> Database a -> Database a
 delete a [] = error "not in Database"
 delete a xs = deleteAUX a xs []
@@ -27,15 +42,30 @@ delete a xs = deleteAUX a xs []
           | j == (fst y) = n ++ ys
           | otherwise = deleteAUX j ys (n++[y])
 
+{- insert
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 insert :: a -> Id -> Database a -> Database a
 insert a i xs = (a,i):xs
 
+{- grab
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 grab :: Eq a => a -> Database a -> a
 grab a [] = error "not in Database"
 grab a (x:xs)
   | a == (fst x) = fst x
   | otherwise = grab a xs
 
+{- grabWithId
+   PRE:  True
+   POST: Returns the argument name
+   SIDE EFFECTS: ... if any, including exceptions ...
+-}
 grabWithId :: Id -> Database a -> a
 grabWithId i [] = error "not in our database"
 grabWithId i (x:xs)
