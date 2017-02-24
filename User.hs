c4ea824da6c782d58f815a0264e0e5533e702952
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-24 13:06:50 +0100

    Wrote Coding convention

--- User.hs
+++ User.hs
@@ -11,60 +11,56 @@ type IsAdmin = Bool
 
 data User = Void | User Name Id Wallet Spent IsAdmin deriving (Show, Eq)
 
-{- newUser
+{- newUser a b c d e
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: returns a new User from the variables a b c d
 -}
 newUser :: Name -> Id -> Wallet -> Spent -> IsAdmin -> User
 newUser a b c d e = User a b c d e
 
-{- setName
+{- setName x u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: returns a new user with the name x
 -}
 setName :: Name -> User -> User
 setName x (User c i wallet spent a ) = User x i wallet spent a
 
-{- getName
+{- getName u
    PRE:  True
-   POST: Returns the argument name
+   POST: Returns the name of user u
    SIDE EFFECTS: ... if any, including exceptions ...
 -}
 getName :: User -> Name
 getName (User c i wallet spent a ) = c
 
-{- setId
+{- setId i u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns a user u with a uppdated Id to i
 -}
 setId :: Id -> User -> User
 setId x (User c i wallet spent a ) = User c x wallet spent a
 
-{- getId
+{- getId u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the id of user u
 -}
 getId :: User -> Id
 getId (User c i wallet spent a ) = i
 
-{- fillWallet
+{- fillWallet w u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the user u with a wallet now containing w more currency
+   SIDE EFFECTS: in case of a negative w we return an error.
 -}
 fillWallet :: Wallet -> User -> User
 fillWallet x (User c i wallet spent a )
   | x > 0 = User c i (wallet + x) spent a
   | otherwise = error "You can't fill with negative currency"
 
-{- removeWallet
+{- removeWallet w u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the user u with a wallet now containing w less currency
+   SIDE EFFECTS: in case of w being larger that what user aldready has. or if w is negative.
 -}
 removeWallet :: Wallet -> User -> User
 removeWallet x (User c i wallet spent a )
@@ -72,52 +68,59 @@ removeWallet x (User c i wallet spent a )
   | x > 0 = User c i (wallet - x) spent a
   | otherwise = error "Invalid number"
 
-{- clearWallet
+{- clearWallet u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: returns the user u with a empty wallet!
 -}
 clearWallet :: User -> User
 clearWallet (User c i wallet spent a ) = User c i 0 spent a
 
-{- getWallet
+{- getWallet u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Return the wallet of user u
 -}
 getWallet :: User -> Wallet
 getWallet (User c i wallet spent a ) = wallet
 
-{- addSpent
+{- addSpent s u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: returns the user u with its (orignal spent value + s)
 -}
 addSpent :: Spent -> User -> User
 addSpent x (User c i wallet spent a)
-  | x > 0 = User c i wallet (spent + x) a
+  | x >= 0 = User c i wallet (spent + x) a
   | x < 0 = error "The value must be greater than zero"
 
-{- removeSpent
+{- reduceSpent s u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the user u with its (original spent value - s)
+   SIDE EFFECTS:
+   if s is larger than what the user already spent you get an error.
+   if s is lesser than 0 we also get an error.
+-}
+reduceSpent :: Spent -> User -> User
+reduceSpent x (User c i wallet spent)
+  | x >= 0 && x <= spent = User c i wallet (spent - x)
+  | x < 0                = error "The value myst be greater than zero"
+  | otherwise            = error "You can't reduce spent with more than spent already is"
+
+{- removeSpent s u
+   PRE:  True
+   POST: Returns the user u with its 0 spent.
 -}
 removeSpent :: User -> User
 removeSpent (User c i wallet spent a) = User c i wallet 0 a
 
-{- makeAdmin
+{- makeAdmin u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the user u with a updated adminstatus
 -}
 makeAdmin ::User -> User
 makeAdmin (User c i wallet spent a) = User c i wallet spent True
 
-{- removeAdmin
+{- removeAdmin user u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the with a updated adminstatus
 -}
 removeAdmin ::User -> User
 removeAdmin (User c i wallet spent a) = User c i wallet spent False
