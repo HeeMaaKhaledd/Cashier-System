e66f8cc3d02531ca2d6e5530eff3b17f63b299f2
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-24 11:53:34 +0100

    wrote almost complete codeconvetion

--- Cart.hs
+++ Cart.hs
@@ -6,52 +6,41 @@ type Cart = [Item]
 
 {- empty
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the empty Cart
 -}
 empty :: Cart
 empty = []
 
-{- addToCart
+{- addToCart i c
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: returns a new cart that now also containing the item i
 -}
 addToCart :: Item -> Cart -> Cart
 addToCart i c = [i] ++ c
 
-{- removeFromCart
+{- removeFromCart i c
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns the new cart but now without i in it.
+   SIDE EFFECTS: error if item is not in the list. ?? doublecheck
 -}
 removeFromCart :: Item -> Cart -> Cart
 removeFromCart i [] = error "non existing product"
 removeFromCart i c = removeFromCartAUX i c Cart.empty
 
-{- removeFromCartAUX
+{- removeFromCartAUX i c u
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns a new cart without i in it.
 -}
 removeFromCartAUX :: Item -> Cart -> Cart -> Cart
 removeFromCartAUX i (c:cs) newC
   | i == c = newC ++ cs
   | i /= c = removeFromCartAUX i cs (c:newC)
 
-  {- calculatePrice
-     PRE:  True
-     POST: Returns the argument name
-     SIDE EFFECTS: ... if any, including exceptions ...
-  -}
-calculatePrice :: Cart -> Int
-calculatePrice [] = 0
-calculatePrice (c:cs) = getPrice c + calculatePrice cs
-
 {- getFirst
    PRE:  True
-   POST: Returns the argument name
-   SIDE EFFECTS: ... if any, including exceptions ...
+   POST: Returns a tuple with the first item and the remaining Cart
+   SIDE EFFECTS: returns a error if cart is empty -- check how to write Side Effects
 -}
 getFirst :: Cart -> (Item,Cart)
+getFirst [] = error "no items in Cart"
 getFirst (c:cs) = (c,cs)
