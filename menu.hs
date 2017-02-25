355a0c7848d47ef7c277300b176dbb53e5b2f5c4
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-25 15:30:47 +0100

    Wrote GUI

--- menu.hs
+++ menu.hs
@@ -12,7 +12,6 @@ scndMain i message = do
   putStrLn (show (Interface.getCart i))
   putStrLn "---------------------------"
   putStrLn "==========================="
-  putStrLn "-- 0 -- GET ITEMS LIST   --"
   putStrLn "-- 1 -- ADD TO CART      --"
   putStrLn "-- 2 -- REMOVE FROM CART --"
   putStrLn "-- 3 -- BUY CART         --"
@@ -21,7 +20,8 @@ scndMain i message = do
   putStrLn "--   --                  --"
   putStrLn "--   --                  --"
   putStrLn "--   --                  --"
-  putStrLn "-- 9 -- ADMIN ONLY       --"
+  putStrLn "--   --                  --"
+  putStrLn "-- 0 -- ADMIN ONLY       --"
   putStrLn "==========================="
   putStrLn "---------------------------"
   putStrLn message
@@ -33,18 +33,6 @@ scndMain i message = do
   run (read x :: Int) i
 
 run c i
-  | c == 0 = do
-    system "clear"
-    putStrLn "---------------------------"
-    putStrLn "--     ITEM IN SHOP      --"
-    putStrLn "---------------------------"
-    mapM_ putStrLn (map show(Interface.getDatabaseItem i))
-    putStrLn "---------------------------"
-    putStrLn "--     Type anything     --"
-    putStrLn "---------------------------"
-    x <- getLine
-    scndMain i ""
-
   | c == 1 = do
     system "clear"
     putStrLn "---------------------------"
@@ -75,29 +63,57 @@ run c i
 
   | c == 3 = do
     scndMain (buy i) ("You bought the following Cart " ++ (show (Interface.getCart i)))
-  | c == 9 = undefined
 
+  | c == 0 && (Interface.getUserAdmin i) = adminMain i "Access Granted"
+  | c == 0 = scndMain i "You dont have access to admin features!"
+  | otherwise = scndMain i "You wrote a non existing number"
 
+adminMain i message = do
+  system "clear"
+  putStrLn "---------------------------"
+  putStrLn (show (Interface.getUser i))
+  putStrLn "---------------------------"
+  putStrLn (show (Interface.getCart i))
+  putStrLn "---------------------------"
+  putStrLn "==========================="
+  putStrLn "-- 1 -- USER FIXES       --"
+  putStrLn "-- 2 -- ITEM FIXES       --"
+  putStrLn "-- 3 -- CART FIXES       --"
+  putStrLn "-- 4 -- DATABASE FIXES   --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "-- 0 -- NAVIGATE BACK    --"
+  putStrLn "==========================="
+  putStrLn "---------------------------"
+  putStrLn message
+  putStrLn "---------------------------"
+  putStrLn "-- Pick your alternative --"
+  putStrLn "---------------------------"
 
--- scndMain interface = do
---   putStrLn "What do you want to do?"
---   putStrLn "[1]Buy item"
---   putStrLn "[2]Insert item"
---   putStrLn "[3]Remove item"
---   x <- getLine
---   run (read x :: Int) interface
---
--- run choice interface
---   | choice == 1 = do
---         putStrLn "enter EAN code"
---         x <- getLine
---         scndMain Interface.buy (findItem (read x :: Int) interface) interface
---
---   | choice == 2 = do
---         putStrLn "Good input"
---   | choice == 3 = do
---         putStrLn "Good input"
-
-  -- | otherwise = do
-  --         putStrLn "Bad input, try again"
-  --         run (read x :: Int)
+adminUserMain i message = do
+  system "clear"
+  putStrLn "---------------------------"
+  putStrLn (show (Interface.getUser i))
+  putStrLn "---------------------------"
+  putStrLn (show (Interface.getCart i))
+  putStrLn "---------------------------"
+  putStrLn "==========================="
+  putStrLn "-- 1 -- USER FIXES       --"
+  putStrLn "-- 2 -- ITEM FIXES       --"
+  putStrLn "-- 3 -- CART FIXES       --"
+  putStrLn "-- 4 -- DATABASE FIXES   --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "--   --                  --"
+  putStrLn "-- 0 -- NAVIGATE BACK    --"
+  putStrLn "==========================="
+  putStrLn "---------------------------"
+  putStrLn message
+  putStrLn "---------------------------"
+  putStrLn "-- Pick your alternative --"
+  putStrLn "---------------------------"
