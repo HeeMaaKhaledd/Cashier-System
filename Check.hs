df60c1f9a47bfbd08fcdd43fc8b6e0731cbb9c47
Grim Mostrom <grimmostrom@ext153-177.eduroam.uu.se>
2017-02-27 17:28:44 +0100

    precond

--- Check.hs
+++ Check.hs
@@ -1,9 +1,22 @@
 module Check(checkIfOnlyInt,checkIfBool) where
-
+{- checkIfOnlyInt s
+   PRE:  True
+   POST: returns a boolean True/False wether the string contains something else than integers
+   EXAMPLES: checkIfOnlyInt "1123" = True
+             checkIfOnlyInt "a123" = False
+             checkIfOnlyInt [] = False
+  -}
 checkIfOnlyInt :: String -> Bool
 checkIfOnlyInt [] = False
 checkIfOnlyInt s = checkIfOnlyIntAUX s
-
+{- checkIfOnlyIntAUX x
+   PRE:  True
+   POST: determines if each x is an integer, else False
+   VARIANT: length of x
+   EXAMPLES: checkIfOnlyIntAUX "1123" = True
+             checkIfOnlyIntAUX "a123" = False
+             checkIfOnlyIntAUX [] = True
+-}
 checkIfOnlyIntAUX :: String -> Bool
 checkIfOnlyIntAUX [] = True
 checkIfOnlyIntAUX (x:xs)
@@ -18,7 +31,13 @@ checkIfOnlyIntAUX (x:xs)
   | x == '8'  = checkIfOnlyIntAUX xs
   | x == '9'  = checkIfOnlyIntAUX xs
   | otherwise = False
-
+{- checkIfBool
+   PRE:  True
+   POST: determines if each s is a Boolean. else False
+   EXAMPLES: checkIfBool "True" = True
+             checkIfBool "False" = True
+             checkIfBool "asd" = False
+-}
 checkIfBool :: String -> Bool
 checkIfBool s
   | s == "True"  = True
