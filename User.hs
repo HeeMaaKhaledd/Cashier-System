8277833810a68e7d9e1ba37274622dea7b641ad4
Sebastian Lhådö <Sebbelhado@gmail.com>
2017-02-27 16:44:23 +0100

    ..

--- User.hs
+++ User.hs
@@ -17,7 +17,7 @@ type IsAdmin = Bool
 
    REPRESENTATION INVARIANT: Wallet & Spent can not be a negative number!
  -}
-data User = User Name Id Wallet Spent IsAdmin deriving (Show, Eq)
+data User = Void | User Name Id Wallet Spent IsAdmin deriving (Show, Eq)
 
 empty :: User
 empty = Void
